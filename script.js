1527b46d6ae11efe8d71e9dcda55c2caaead6b9a
ilkim berfin kayar <ilkimberfink@outlook.com>
2023-03-14 16:54:02 -0500

    script change

--- script.js
+++ script.js
@@ -36,25 +36,3 @@ window.addEventListener("load", function () {
         addDestinationInfo(document, selection.name, selection.diameter, selection.star, selection.distance, selection.moons, selection.image);
     })
 });
-    
-
-    
-
-/* window.addEventListener("load", function() {
-
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
-
-
-   
-}); */
-
