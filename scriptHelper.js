d2e25a3ae97a466f87e5e96c44bc10b2f5c33055
ilkim berfin kayar <ilkimberfink@outlook.com>
2023-03-14 20:48:55 -0500

    trial...

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,7 +2,6 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
         let finalMissionDestitnation = document.getElementById("missionTarget");
                 finalMissionDestitnation.innerHTML = `
                  <h2>Mission Destination</h2>
@@ -13,10 +12,10 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                      <li>Distance from Earth: ${distance} </li>
                      <li>Number of Moons: ${moons} </li>
                  </ol>
-                 <img src="${imageUrl}">
+                 <img src="${imageUrl}"></img>
                  `
     
-}
+} 
 
 function validateInput(testInput) {
     if (testInput == "") {
