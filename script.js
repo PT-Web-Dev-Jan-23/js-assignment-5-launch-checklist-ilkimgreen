9fbea0452a6f8595a6354138a83e5ce07fa1658c
ilkim berfin kayar <ilkimberfink@outlook.com>
2023-03-16 19:21:43 -0500

    the finalist commit

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ window.addEventListener("load", function() {
     
     form.addEventListener("submit", function(event) {
         event.preventDefault();
-        let pilotName = document.querySelector("input[name=pilotName]").value;
-        let copilotName = document.querySelector("input[name=copilotName]").value;
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
         let cargoLevel = document.querySelector("input[name=cargoMass]").value;
         
